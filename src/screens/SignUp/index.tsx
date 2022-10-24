8c075c39030555e88e351bfbf3130090cc3bc87a
markus90souza <markus90souza@gmail.com>
2022-10-24 18:01:43 -0300

    feat -> layout: create signUp screen and finish

---
+++ src/screens/SignUp/index.tsx
@@ -0,0 +1,63 @@
+import { Center, Heading, Image, ScrollView, Text, VStack } from 'native-base'
+
+import bgCover from '@assets/background.png'
+import Logo from '@assets/logo.svg'
+import { Input } from '@components/Input'
+import { Button } from '@components/Button'
+
+const SignUp = () => {
+  return (
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{ flexGrow: 1 }}
+    >
+      <VStack flex={1} safeArea bgColor={'gray.700'} px={10}>
+        <Image
+          source={bgCover}
+          alt={'Pessoas trienandos'}
+          position={'absolute'}
+          resizeMode={'contain'}
+        />
+
+        <Center marginY={24}>
+          <Logo />
+          <Text color={'gray.100'} fontSize={'sm'}>
+            Treine sua mente e sue corpo
+          </Text>
+        </Center>
+        <Center>
+          <Heading
+            fontFamily={'heading'}
+            fontSize={'xl'}
+            color={'gray.100'}
+            mb={6}
+          >
+            Crie sua conta
+          </Heading>
+
+          <Input placeholder="Nome" mb={4} autoCorrect={false} />
+
+          <Input
+            placeholder="E-mail"
+            mb={4}
+            autoCapitalize={'none'}
+            autoCorrect={false}
+            keyboardType={'email-address'}
+          />
+
+          <Input placeholder="Senha" mb={4} secureTextEntry />
+
+          <Button name={'Criar e acessar'} />
+        </Center>
+
+        <Button
+          marginTop={24}
+          name={'Voltar para o login'}
+          variant={'outline'}
+        />
+      </VStack>
+    </ScrollView>
+  )
+}
+
+export { SignUp }
